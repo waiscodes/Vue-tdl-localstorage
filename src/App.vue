3b883ed2ca8f9a92f2092e7baba4970999f44e69
waiscodes <b.wais@outlook.com>
2021-01-04 11:43:52 -0700

    imported to do list into list into app and deleted helloworld

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <ToDoList yourName="Birm" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ToDoList from "./components/ToDoList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ToDoList,
   },
 };
 </script>
